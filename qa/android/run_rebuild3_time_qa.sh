daa6406c074781901eb11eef3129c2669d5abe08
aafanasevaannaz-cyber <aafanasevaannaz@gmail.com>
2026-07-29 15:05:39 +0300

    Use cached UI-tree coordinate for time-step continuation

--- qa/android/run_rebuild3_time_qa.sh
+++ qa/android/run_rebuild3_time_qa.sh
@@ -24,12 +24,16 @@ replacements = {
         "grep -E 'android:id/time_header|android:id/radial_picker|android:id/hours|android.widget.TimePicker|android.widget.NumberPicker'",
     'QUICK_TWELVE="$(coords_for "$LAST_XML" --text "Приём 1, установить 12:00" --index 0)"':
         'PLUS_FIFTEEN="$(coords_for "$LAST_XML" --text "Приём 1, плюс 15 минут" --index 0)"',
+    'MINUS_FIFTEEN="$(coords_for "$LAST_XML" --text "Приём 1, минус 15 минут" --index 0)"':
+        'MINUS_FIFTEEN="$(coords_for "$LAST_XML" --text "Приём 1, минус 15 минут" --index 0)"\nCONTINUE_COORD="$(coords_for "$LAST_XML" --text "Продолжить" --index 0)"',
     'for required in "$COUNT_TWO" "$COUNT_THREE" "$COUNT_FOUR" "$TIME_ONE" "$QUICK_TWELVE" "$MINUS_FIFTEEN"; do':
-        'for required in "$COUNT_TWO" "$COUNT_THREE" "$COUNT_FOUR" "$TIME_ONE" "$PLUS_FIFTEEN" "$MINUS_FIFTEEN"; do',
+        'for required in "$COUNT_TWO" "$COUNT_THREE" "$COUNT_FOUR" "$TIME_ONE" "$PLUS_FIFTEEN" "$MINUS_FIFTEEN" "$CONTINUE_COORD"; do',
     'tap_coords "$QUICK_TWELVE" "быстро установить 12:00"\ntap_coords "$MINUS_FIFTEEN" "уменьшить на 15 минут"':
         'for press in $(seq 1 15); do\n  tap_coords "$PLUS_FIFTEEN" "увеличить время на 15 минут (${press}/15)"\ndone',
     'assert_text "${XML_DIR}/03-times-final.xml" "11:45" "Время не изменилось без клавиатуры"':
         'log_action "Пятнадцать быстрых изменений выполнены; итог 11:45 проверяется после сохранения"',
+    'scroll_and_tap "Продолжить" "03-times-continue" 12 || exit 1':
+        'tap_coords "$CONTINUE_COORD" "Продолжить после настройки времени"',
 }
 for old, new in replacements.items():
     if old not in text:
